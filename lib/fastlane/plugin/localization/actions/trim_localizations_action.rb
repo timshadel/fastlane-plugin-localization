1e59beebf05e3654d94e467b1fd2cb758fcce2cc
Tim Shadel <tim@shadelsoftware.com>
2018-07-19 10:54:43 -0600

    Add support for removing files or ids

--- lib/fastlane/plugin/localization/actions/trim_localizations_action.rb
+++ lib/fastlane/plugin/localization/actions/trim_localizations_action.rb
@@ -14,8 +14,71 @@ module Fastlane
 
         UI.success "Trimming localizations in #{path}"
 
-        matches = []
         doc = Oga.parse_xml(File.open(path))
+
+        # Remove files
+
+        matches = []
+        params[:files].each do |e|
+          if e.kind_of? String
+            doc.css("file").select { |f| f.attr('original').value == e }.each do |node|
+              matches << Match.new(node, 'file', e)
+            end
+          elsif e.kind_of? Regexp
+            doc.css("file").select { |f| f.attr('original').value =~ e }.each do |node|
+              matches << Match.new(node, 'file', e.to_s)
+            end
+          else
+            UI.error "Trimming value (#{e}) must be either a string or a regular expression."
+          end
+        end
+
+        matches.each do |match|
+          puts(match) if match.node.nil?
+          file = match.node
+          UI.message "Removing file #{file.attr('original').value} because its path matched '#{match.exp}'"
+          file.remove
+        end
+
+        if matches.count == 1
+          UI.success "Trimmed 1 file"
+        else
+          UI.success "Trimmed #{matches.count} files"
+        end
+
+
+        matches = []
+        params[:ids].each do |e|
+          if e.kind_of? String
+            doc.css("trans-unit").select { |u| u.attr('id').value == e }.each do |node|
+              matches << Match.new(node, 'phrase', e)
+            end
+          elsif e.kind_of? Regexp
+            doc.css("trans-unit").select { |u| u.attr('id').value =~ e }.each do |node|
+              matches << Match.new(node, 'phrase', e.to_s)
+            end
+          else
+            UI.error "Trimming value (#{e}) must be either a string or a regular expression."
+          end
+        end
+
+        matches.each do |match|
+          puts(match) if match.node.nil?
+          unit = match.node
+          UI.message "Removing phrase #{unit.attr('id').value} because its id matched '#{match.exp}'"
+          unit.remove
+        end
+
+        if matches.count == 1
+          UI.success "Trimmed 1 localization"
+        else
+          UI.success "Trimmed #{matches.count} localizations"
+        end
+
+
+        # Remove individual items
+
+        matches = []
         items.each do |type, values|
           kind = type == :source ? 'text' : 'comment'
           values.each do |e|
@@ -68,6 +131,14 @@ module Fastlane
                                 description: "XLIFF file to be trimmed",
                                    optional: false,
                                        type: String),
+           FastlaneCore::ConfigItem.new(key: :files,
+                                description: "Entries originating from these files will be removed from the XLIFF file",
+                                   optional: true,
+                                       type: Array),
+           FastlaneCore::ConfigItem.new(key: :ids,
+                                description: "Entries with these ids will be removed from the XLIFF file",
+                                   optional: true,
+                                       type: Array),
            FastlaneCore::ConfigItem.new(key: :text,
                                 description: "Sources that match these strings or expressions will be removed from the XLIFF file",
                                    optional: true,
